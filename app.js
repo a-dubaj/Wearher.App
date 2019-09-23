a8db9a6a181abbf9254191d7404761db7e6357e4
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2019-09-24 00:23:59 +0200

    Fix typos

--- app.js
+++ app.js
@@ -1,28 +1,31 @@
-window.addEventListener('load', ()=> {
+window.addEventListener("load", ()=> {
     let long;
     let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
+    
+    let temperatureDescription = document.querySelector(".temperature-description");
+    let temperatureDegree = document.querySelector(".temperature-degree");
+    let locationTimezone = document.querySelector(".location-timezone");
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude; 
             lat = position.coords.latitude;
-            const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+            const proxy = "https://cors-anywhere.herokuapp.com/";
             const api = `${proxy}https://api.darksky.net/forecast/d47007c28897dd799b93b29c2da7cc77/${lat},${long}`;
             
             fetch(api)
-            .then(response => {
-                return response.json();
+                .then(response => {
+                    return response.json();
             })
             .then(data => {
                 console.log(data);
-                const {temperature, summary} = data.currently;
+                const { temperature, summary } = data.currently;
                 //Set DOM Elements from the API
-            })
+                temperatureDegree.textContent = temperature;
+                temperatureDescription.textContent = summary;
+                locationTimezone.textContent = data.timezone;
+            });
         });
-    }else{
-        h1.textContent = "This is not working" 
     }
 });
