ab49edab51e2b5586ee7049b080be2e26865ff6c
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2019-09-23 21:35:41 +0200

    Add changes for API and fatching data

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 window.addEventListener('load', ()=> {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
+
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude; 
             lat = position.coords.latitude;
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const api = `${proxy}https://api.darksky.net/forecast/d47007c28897dd799b93b29c2da7cc77/${lat},${long}`;
+            
+            fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                const {temperature, summary} = data.currntly;
+                //Set DOM Elements from the API
+            })
         });
     }else{
         h1.textContent = "This is not working" 
